5f556bc61a7d2f25d0da74d787f9eb38b011c218
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-11-30 03:40:13 +0100

    islower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def islower(c):
     if ord(c) < 97 or ord(c) > 122:  # check if character is lowercase
         return False  # character is not lowercase
