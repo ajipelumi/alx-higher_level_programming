a0437c531cdd139bdf9790886c9eae241184e6e5
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-12-01 01:14:01 +0100

    uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@ def uppercase(str):
     for letter in str:
         char = ord(letter)  # get character ASCII
         if char >= 97 and char <= 122:  # character is lowercase
-            letter = chr(char - 32)  # convert to uppercase and back to character
+            letter = chr(char - 32)  # convert to uppercase and to character
         print("{}".format(letter), end='')  # print character
     print()  # print newline
