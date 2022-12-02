ca1cb349a4a2a97b241686ded46e57c091874fca
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-12-02 02:01:07 +0100

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import add_0 as addition
+if __name__ == "__main__":
+    import add_0 as addition
 
-a = 1
-b = 2
-res = addition.add(a, b)
-print("{:d} + {:d} = {:d}".format(a, b, res))
+    a = 1
+    b = 2
+    res = addition.add(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, res))
