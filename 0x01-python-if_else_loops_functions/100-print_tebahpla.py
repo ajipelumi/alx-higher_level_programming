9388d1b8d76776f2640357adb7c9f530096c7d15
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-12-03 00:59:13 +0100

    alphabet in reverse

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,7 +3,7 @@
 for i in range(122, 96, -1):
     if i % 2 == 0:
         # print lowercase
-        print ("{}".format(chr(i)), end="")
+        print("{}".format(chr(i)), end="")
     else:
         # print uppercase
-        print ("{}".format(chr(i - 32)), end="")
+        print("{}".format(chr(i - 32)), end="")
