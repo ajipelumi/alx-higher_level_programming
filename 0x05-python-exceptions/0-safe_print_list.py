f96a6ba44de28005ab2f542d7f91ecea2ddad655
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-12-19 22:43:27 +0100

    safe print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    """
+    prints x elements of a list
+    my_list: contain any type (integer, string, etc.)
+    x: represents the number of elements to print
+    Return: the real number of elements printed
+    """
     try:
         count = 0
         for item in my_list:  # loop through list
