fe094439a7917ceaec8c22069cc0f905e7c57a07
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-12-21 00:30:57 +0100

    print with space

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+""" This module defines a square. """
+
+
+class Square:
+    """ Defines a square. """
+    def __init__(self, size=0, position=(0, 0)):
+        """ Initializes the data. """
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """ Retrieve size. """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Set property. """
+        self.__size = value
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def position(self):
+        """ Retrieve position. """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """ Set property. """
+        self.__position = value
+        if type(value[0]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    def area(self):
+        """ Returns the current square area. """
+        return self.__size ** 2
+
+    def my_print(self):
+        """ Prints in stdout the square with the character #. """
+        num = self.__size
+        pos = self.__position
+        if num == 0:
+            print()  # newline
+        for i in range(num):
+            count = 0
+            for j in range(num):
+                for k in range(pos[0]):
+                    if count is not pos[0]:
+                        print(" ", end="")  # fill with spaces
+                count = pos[0]
+                print("#", end="")
+            print()  # newline
