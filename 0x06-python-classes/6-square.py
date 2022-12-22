16114e785a12c2eca5b9488a28b9bc3e6ea5a4f8
ajipelumi <ajisafeoluwapelumi@gmail.com>
2022-12-22 21:33:11 +0100

    square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,8 +6,8 @@ class Square:
     """ Defines a square. """
     def __init__(self, size=0, position=(0, 0)):
         """ Initializes the data. """
-        self.size = size
-        self.position = position
+        self.__size = size
+        self.__position = position
 
     @property
     def size(self):
