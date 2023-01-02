210db0d41c073c8871f7d062b37a4aceb7c89f79
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-01-02 20:27:52 +0100

    str

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -43,7 +43,7 @@ class Rectangle:
 
     def perimeter(self):
         """ Returns the rectangle perimeter. """
-        if self.__width == 0 and self.__height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         else:
             return 2 * (self.width + self.height)
@@ -51,7 +51,7 @@ class Rectangle:
     def __str__(self):
         """ Returns a string to print to STDOUT. """
         str = ''
-        if self.__width == 0 and self.__height == 0:
+        if self.__width == 0 or self.__height == 0:
             return str
 
         for i in range(self.__height):
