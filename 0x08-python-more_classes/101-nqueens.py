e31709c5cd532def93dc92247ec073be98ee662a
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-01-05 11:04:41 +0100

    n queens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -74,14 +74,16 @@ if len(sys.argv) != 2:  # wrong number of arguments
     print("Usage: nqueens N")
     sys.exit(1)
 
-num = int(sys.argv[1])  # cast as integer
+num = sys.argv[1]
 
-if type(num) != int:  # N must be an int
+try:
+    num_int = int(num)
+except ValueError:  # N must be an int
     print("N must be a number")
     sys.exit(1)
 
-if num < 4:  # N must be greater than or equal to 4
+if num_int < 4:  # N must be greater than or equal to 4
     print("N must be at least 4")
     sys.exit(1)
 
-solve_n_queens(num)
+solve_n_queens(num_int)
