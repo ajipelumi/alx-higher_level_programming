fead593eb91ae557fcaf8612f43e0df52df6d7c9
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-01-09 19:49:17 +0100

    lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+""" Returns the list of available attributes and methods of an object. """
+
+
 def lookup(obj):
-    """ Returns the list of available attributes and methods of an object. """
+    """
+    gets the attributes and methods of an object with dir.
+    obj: object to be scanned
+    return a list of the attributes and objects
+    """
     return list(dir(obj))
