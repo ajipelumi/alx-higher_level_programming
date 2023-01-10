3ac741a542b59f274e42b771c99265e6d7c5c857
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-01-10 23:57:39 +0100

    student class

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Defines a student. """
+
+
+class Student:
+    """ Student class. """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize data.
+        @first_name: first name of student
+        @last_name: last name of student
+        @age: student's age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ Retrieves a dictionary representation of a Student instance. """
+        return self.__dict__
