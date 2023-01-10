cbe7722891ca1d302f321f6b442eaff1d48044c4
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-01-10 20:34:36 +0100

    load, add, save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Adds all arguments to a Python list, and then save them to a file. """
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
+import sys
+
+
+# Create a list
+my_list = []
+
+# Get length of arguments passed
+arg_len = len(sys.argv)
+
+# Loop through arg and append command line arguments to list
+for item in range(1, arg_len):
+    my_list.append(sys.argv[item])
+
+file = "add_item.json"  # JSON file
+
+# Get object from JSON file
+try:
+    # file exists so we get objects from JSON file
+    obj = load_from_json_file(file)
+    # Append new arguments to returned object
+    new_list = obj + my_list
+    # Save the new list in JSON file
+    save_to_json_file(new_list, file)
+except Exception:
+    # file does not exist so save our arguments to JSON file
+    save_to_json_file(my, file)
