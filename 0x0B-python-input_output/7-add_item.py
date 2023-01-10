f3f7427f12fabdf35df2c100bf28820037b8702d
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-01-10 20:43:27 +0100

    load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Adds all arguments to a Python list, and then save them to a file. """
+
 from 5-save_to_json_file import save_to_json_file
 from 6-load_from_json_file import load_from_json_file
 import sys
