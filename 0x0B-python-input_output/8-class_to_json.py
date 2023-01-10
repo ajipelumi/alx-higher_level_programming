7b032dd113518d9119967fbfce5911e50a66db60
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-01-10 23:20:08 +0100

    class to json

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" JSON serialization of an object. """
+import json
+
+
+def class_to_json(obj):
+    """
+    dictionary description with simple data structure
+    @obj: is an instance of a Class
+    Return: the dictionary description.
+    """
+    # Get instance attributes/variables with built-in (vars)
+    new = json.dumps(vars(obj))
+    return new
