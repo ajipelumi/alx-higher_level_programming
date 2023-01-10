fad8904fcdf7363ad35f500d97493d8f2c69e654
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-01-10 03:07:26 +0100

    square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Square class that inherits from Rectangle. """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Square Class. """
+    def __init__(self, size):
+        """ Initialize data. """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """ Area of a square. """
+        return self.__size ** 2
