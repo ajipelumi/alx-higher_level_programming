d3cc6ed27777d954682855b38287949520601cbd
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-01-10 20:49:18 +0100

    add

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""
+Script adds all arguments to a Python list, and then save them to a file.
+"""
 
+import sys
 from 5-save_to_json_file import save_to_json_file
 from 6-load_from_json_file import load_from_json_file
-import sys
 
 
 # Create a list
