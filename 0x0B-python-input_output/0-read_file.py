69cfd83f9c4ad6a853b8e8afcd1df63aa4d138eb
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-01-10 18:40:29 +0100

    read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,5 @@
 def read_file(filename=""):
     """ Reads a text file. """
     with open(filename, "r", encoding="utf-8") as f:
-        print(f.read())
+        for line in f:
+            print(line, end='')
