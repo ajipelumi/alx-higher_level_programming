48f5558f653b887ad0a920a77006ef7940a29bd0
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-01-11 16:25:58 +0100

    reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" Defines a student. """
+
+
+class Student:
+    """ Student class. """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize data.
+        @first_name: first name of student
+        @last_name: last name of student
+        @age: student's age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Retrieves a dictionary representation of a Student instance. """
+        # Define a dictionary to store items
+        my_dict = {}
+
+        # Check for list of keys
+        if attrs is None:  # No list found
+            return self.__dict__
+        else:  # List found
+            for item in attrs:  # Iterate through list
+                if item in self.__dict__:  # Key found
+                    item_dict = {item: self.__dict__[item]}
+                    # Update dictionary everytime key is found
+                    my_dict.update(item_dict)
+            return my_dict
+
+    def reload_from_json(self, json):
+        """ Replaces all attributes of the Student instance. """
+        # Instance now holds the items in json
+        self.__dict__ = json
