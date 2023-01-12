27ecdd789ee61e3c8f9672830276b11c5f0904bc
Ajisafe Oluwapelumi <106672692+ajipelumi@users.noreply.github.com>
2023-01-12 12:57:44 +0100

    add attribute

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -8,5 +8,9 @@ def add_attribute(a, key="", value=""):
     if isinstance(a, (str, tuple, int, float, bool)):
         raise TypeError("can't add new attribute")
 
+    # Check if class has __slots__ defined
+    if hasattr(a, "__slots__") and key not in a.__slots__:
+        raise TypeError("can't add new attribute")
+
     # Set attribute with setattr built-in
     setattr(a, key, value)
