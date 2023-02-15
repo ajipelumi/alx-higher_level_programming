d91118d80b413c70569661c13a5e85a0767386eb
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-02-16 00:25:58 +0100

    insert

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,3 @@
 -- This script inserts a new row in the table first_table from the database hbtn_0c_0 in our MySQL server.
-INSERT INTO first_table
-(id, name)
-VALUES(89, 'Best School');
+INSERT INTO first_table (id, name) VALUES
+(89, 'Best School');
