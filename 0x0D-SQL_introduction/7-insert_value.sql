5f1c21b687011d531c8b613e725f19c273e0c552
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-02-16 02:14:37 +0100

    no limk

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,4 @@
 -- This script inserts a new row in the table first_table from the database hbtn_0c_0 in our MySQL server.
-INSERT INTO first_table (id, name) VALUES
-(89, 'Best School');
+INSERT INTO second_table (score, name) VALUES
+(18, 'Aria'),
+(12, 'Aria');
