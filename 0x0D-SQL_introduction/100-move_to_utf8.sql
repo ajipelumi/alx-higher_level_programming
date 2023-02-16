89cd788861a823f144518811056e39100e09f3d0
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-02-16 03:41:43 +0100

    utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -10,7 +10,7 @@ ALTER TABLE first_table
 	COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE first_table
-	MODIFY name
+	CHANGE name name
 	VARCHAR(256)
 	CHARACTER SET utf8mb4
 	COLLATE utf8mb4_unicode_ci;
