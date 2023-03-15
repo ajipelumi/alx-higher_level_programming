3287f412e80b107effdb2c85e7466c3fc005c5dd
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-03-15 04:11:04 +0100

    reverse list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  list.reverse();
-  return list; // Return reversed list
+  let len = list.length; // Get list length
+  const newList = []; // New list
+  // Loop through list to store items in reverse
+  for (let i = 0; i < list.length; i++) {
+    newList[i] = list[len - 1];
+    len--;
+  }
+  return newList; // Return reversed list
 };
