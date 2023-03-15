9822fd5413310b8ae722b0f55602f73bf707941b
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-03-15 15:40:04 +0100

    converter

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -2,19 +2,7 @@
 
 exports.converter = function (base) {
   function numConvert (num) {
-    let convertedNumber = ''; // Initialize string to store converted number
-    while (num > 0) {
-      let remainder = num % base; // Get remainder
-      if (remainder > 9) { // Hex conversion
-        const list = ['a', 'b', 'c', 'd', 'e', 'f'];
-        remainder = list[remainder - 10];
-      }
-      convertedNumber += remainder; // Concatenate remainder to string
-      num = Math.floor(num / base); // Divide num by base to continue loop
-    }
-    // Reverse string before returning
-    convertedNumber = convertedNumber.split('').reverse().join('');
-    return (convertedNumber || '0'); // Return converted number
+    return num.toString(base); // Return converted number
   }
   return numConvert; // Return numConvert function
 };
