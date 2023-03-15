2740d2b3a8468b8c056b89f94f341558c5bb4bde
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-03-15 01:45:55 +0100

    class with constructor

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    this.width = w; // Initialize with the value of w
+    this.height = h; // Initialize with the value of h
+  }
+}
+
+module.exports = Rectangle;
