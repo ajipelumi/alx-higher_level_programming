b777e4cfe741af409616cef48ce27aac9173b815
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-03-18 04:31:34 +0100

    adds new state

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Adds a new state object to passed database. """
+""" Displays a state object from passed database. """
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from model_state import Base, State
