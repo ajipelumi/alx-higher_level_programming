3a994f5acaa89b2cf7c3344ed4eccec3702d6a4c
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-03-18 03:06:56 +0100

    contains funtionality

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Lists all states passed from passed database. """
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+import sys
+
+
+if __name__ == '__main__':
+    db_user = sys.argv[1]  # Stores user
+    db_password = sys.argv[2]  # Stores user password
+    db_name = sys.argv[3]  # Stores name of database
+
+    # Create engine and session
+    call = f'mysql+mysqldb://{db_user}:{db_password}@localhost/{db_name}'
+    engine = create_engine(call)  # Establish connection
+    session = Session(engine)  # Establish conversation with database
+
+    # Query database to display states table
+    states = session.query(State).filter(State.name.contains('a'))\
+            .order_by(State.id)
+    for state in states:
+        print(f'{state.id}: {state.name}')
