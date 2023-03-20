6c8da1475cb9e80bc9b3c13e03f949c5fddab1cd
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-03-20 17:50:31 +0100

    list

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -26,4 +26,4 @@ if __name__ == '__main__':
     for state in states:
         print(f'{state.id}: {state.name}')
         for city in state.cities:
-            print(f'    {city.id}: {city.name}')
+            print('\t'f'{city.id}: {city.name}')
