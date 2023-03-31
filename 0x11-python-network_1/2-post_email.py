40b696a6afd138cde2b285fac5f901db23752981
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-03-31 22:54:05 +0100

    post an email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,13 +5,14 @@ request to the passed URL with the email as a parameter,
 and displays the body of the response (decoded in utf-8).
 """
 import sys
-import urllib.request, urllib.parse
+import urllib.request
+import urllib.parse
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    values = {'email' : email}
+    values = {'email': email}
     data = urllib.parse.urlencode(values).encode('utf-8')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
