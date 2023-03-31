3f91e453af4cbcd0d06c64414ca998fd3d524c1d
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-03-31 15:33:43 +0100

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,20 +2,26 @@
 """ Module returns a find_peak function. """
 
 
+def fpeak(my_list, low, high):
+    """ Recursion to find peak. """
+    mid = (low + (high - low) // 2)
+    if mid == 0 and my_list[mid] >= my_list[mid + 1]:
+        return my_list[mid]
+    elif mid == (high - 1) and my_list[mid] >= my_list[mid - 1]:
+        return my_list[mid]
+    elif mid > 0 and my_list[mid - 1] > my_list[mid]:
+        return fpeak(my_list, low, mid - 1)
+    else:
+        return fpeak(my_list, mid + 1, high)
+
+
 def find_peak(list_of_integers):
     """ Finds a peak in a list of unsorted integers."""
-    if list_of_integers is None:
+    if not list_of_integers:
         return None
 
     length = len(list_of_integers)
     if length == 1:
         return list_of_integers[0]
 
-    my_list = list_of_integers
-    for idx, val in enumerate(my_list):
-        if idx == 0 and val >= my_list[idx + 1]:
-            return val
-        elif idx == (length - 1) and val >= my_list[idx - 1]:
-            return val
-        elif val >= my_list[idx + 1] and val >= my_list[idx - 1]:
-            return val
+    return fpeak(list_of_integers, 0, length - 1)
