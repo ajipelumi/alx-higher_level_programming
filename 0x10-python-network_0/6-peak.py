3d88bd0bdaa3691202bab9f0ba30da73fec77c4b
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-03-31 16:07:44 +0100

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,24 +4,35 @@
 
 def fpeak(my_list, low, high):
     """ Recursion to find peak. """
+    # Find middle element
     mid = (low + (high - low) // 2)
-    if mid == 0 and my_list[mid] >= my_list[mid + 1]:
+
+    # If mid is first/last element and not smaller than neighbouring element
+    if (mid == 0 or my_list[mid - 1] <= my_list[mid]) and \
+            (mid == len(my_list) - 1 or my_list[mid + 1] <= my_list[mid]):
         return my_list[mid]
-    elif mid == (high - 1) and my_list[mid] >= my_list[mid - 1]:
-        return my_list[mid]
+
+    # Check if mid is smaller than left element
     elif mid > 0 and my_list[mid - 1] > my_list[mid]:
         return fpeak(my_list, low, mid - 1)
+
+    # Mid is smaller than right element
     else:
         return fpeak(my_list, mid + 1, high)
 
 
 def find_peak(list_of_integers):
     """ Finds a peak in a list of unsorted integers."""
+    # List of integers is empty
     if not list_of_integers:
         return None
 
+    # Get length of list
     length = len(list_of_integers)
+
+    # Check if list contains just one element
     if length == 1:
         return list_of_integers[0]
 
+    # Call recursive fpeak
     return fpeak(list_of_integers, 0, length - 1)
