454236d3905a32e45eb9a6e9b650277c831a66fc
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-03-31 04:09:24 +0100

    body

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script displays the body of the response.
-if [[ $(curl -s -o /dev/null -w "%{http_code}" "$1") == 200 ]]; then curl -s "$1"; fi
+if [[ $(curl -sL -o /dev/null -w "%{http_code}" "$1") == 200 ]]; then curl -sL "$1"; fi
