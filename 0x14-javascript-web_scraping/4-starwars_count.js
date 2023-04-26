53cba9aa64e48b1de9faf11379f3d887430cbc11
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-04-26 23:13:27 +0100

    starwars count

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const argv = require('process').argv;
+const request = require('request');
+
+const url = argv[2];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    let count = 0;
+    const data = JSON.parse(body).results;
+    for (const key in data) {
+      for (const item in data[key].characters) {
+        if (data[key].characters[item] === 'https://swapi-api.alx-tools.com/api/people/18/') {
+          count += 1;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
